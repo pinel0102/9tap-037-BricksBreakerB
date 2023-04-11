fac242afbb6fc0babd615048438dd02623e2cdb8
ks.kim <ks.kim@ninetap.com>
2023-04-11 10:59:50 +0900

    [ks.kim] Build Refresh

---
+++ 103711.Puzzle_BricksBreakerB/Assets/01-GameEngine/Scripts/01.Define/GlobalDefine+Tutorial.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static partial class GlobalDefine
+{
+    public const int TUTORIAL_LEVEL_BRICKS_DELETE = 8;
+    public const int TUTORIAL_LEVEL_EARTHQUAKE = 13;
+    public const int TUTORIAL_LEVEL_ADD_STEEL_BRICKS = 26;
+}
